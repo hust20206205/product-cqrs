b80803894023baa6d2f95ac322dbe29e9ad045f3
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 06:33:01 +0700

    4

---
+++ src/product/core/application/product.application.ts
@@ -0,0 +1,4 @@
+const ProductQueryHandlers: any[] = [];
+const ProductCommandHandlers: any[] = [];
+
+export const ProductApplications = [...ProductQueryHandlers, ...ProductCommandHandlers];
---
+++ src/product/product.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+import { CqrsModule } from "@nestjs/cqrs";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { ProductApplications } from "./core/application/product.application";
+import { ProductInfrastructure } from "./infrastructure/product.infrastructure";
+import { ProductInterface } from "./interface/product.interface";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([...ProductInfrastructure.repositories]), CqrsModule],
+  providers: [...ProductInterface.resolvers, ...ProductInfrastructure.providers, ...ProductApplications],
+  controllers: [...ProductInterface.controllers],
+  exports: [],
+})
+export class ProductModule {}
+
