f23ad205a767e07bb4b5c5adcc4d7d8f7fc69b58
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 06:31:02 +0700

    chore: delete product.application.ts, product.infrastructure.ts, product.interface.ts and product.module.ts

--- src/product.module.ts
+++ src/product.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ProductApplications } from "./core/application/product.application";
-import { ProductInfrastructure } from "./infrastructure/product.infrastructure";
-import { ProductInterface } from "./interface/product.interface";
+import { ProductApplications } from "./product/application/product.application";
+import { ProductInfrastructure } from "./product/infrastructure/product.infrastructure";
+import { ProductInterface } from "./product/interface/product.interface";
 
 @Module({
   imports: [TypeOrmModule.forFeature([...ProductInfrastructure.repositories]), CqrsModule],
