17644d8e0b4242901d102e2cc195308a53eb11aa
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 08:39:45 +0700

    update extensions.json, product.ts and product.factory.ts

--- src/product/core/domain/entities/product.ts
+++ src/product/core/domain/entities/product.ts
@@ -2,9 +2,9 @@ import { ProductName } from '../value-objects/product-name';
 import { AggregateRoot } from './aggregate-root';
 
 export class Product extends AggregateRoot<string> {
+  name: ProductName;
+  createdAt: Date;
   constructor(id: string) {
     super(id);
-    name: ProductName;
-    createdAt: Date;
   }
 }
