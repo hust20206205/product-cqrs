b21c7de974fc1be59e9c5333695e14c83d9b14c7
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 08:45:24 +0700

    update product.ts

--- src/product/core/domain/entities/product.ts
+++ src/product/core/domain/entities/product.ts
@@ -8,3 +8,40 @@ export class Product extends AggregateRoot<string> {
     super(id);
   }
 }
+export class Builder {
+  // private name: ProductName = undefined;
+  // private createdAt: Date = undefined;
+
+  // private constructor(){}
+
+  // public static creer(): Builder { 
+  // return new Builder();
+  // }
+
+  // public static creerDepuisExistant( existant :): Builder { 
+  // let builder = new Builder();
+  // builder.name = existant.name
+  // builder.createdAt = existant.createdAt
+  // return builder;
+  // }
+
+  // public build():  {
+  // return {
+  // "name": this.name,
+  // "createdAt": this.createdAt,
+  // }
+  // }
+
+  // public avecName(v: ProductName): Builder {
+  // this.name= v; //COMMENT INSTANCIER UN NOUVEAU 
+  // return this;
+  // }
+
+  // public avecCreatedAt(v: Date): Builder {
+  // this.createdAt= v; //COMMENT INSTANCIER UN NOUVEAU 
+  // return this;
+  // }
+
+}
+const :  = null; //TODO
+ 
